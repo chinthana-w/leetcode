b2559dbc9c95bedd578c50d099568d0283a57dca
chinthana-w <33711287+chinthana-w@users.noreply.github.com>
2026-03-14 12:23:11 -0500

    Time: 105 ms (58.60%) | Memory: 46 MB (59.24%) - LeetSync

--- 1308-smallest-string-with-swaps/smallest-string-with-swaps.py
+++ 1308-smallest-string-with-swaps/smallest-string-with-swaps.py
@@ -1,8 +1,13 @@
 class Solution:
-    def smallestStringWithSwaps(self, s: str, pairs: List[List[int]]) -> str:
-        graph = build_graph(pairs)
-        groups = build_groups(graph, len(s))
+    def smallestStringWithSwaps(self, s: str, pairs: List[List[int]]) -> str:    
+        root_list = list(range(len(s)))
 
+        for p_1, p_2 in pairs:
+            union(p_1, p_2, root_list)
+
+        groups = get_groups(root_list)
+        groups = [sorted(list(g)) for g in groups.values()]
+        
         s_list = list(s)
 
         for g in groups:
@@ -13,52 +18,28 @@ class Solution:
 
         return ''.join(s_list)
 
-def build_graph(pairs):
-    incidence = {}
+def find(i, root_list):
+    if root_list[i] == i:
+        return i
 
-    for p in pairs:
-        if p[0] in incidence.keys():
-            incidence[p[0]].add(p[1])
+    else:
+        root_list[i] = find(root_list[i], root_list)
+        return root_list[i]
+
+def union(i, j, root_list):
+    root_i = find(i, root_list)
+    root_j = find(j, root_list)
+
+    root_list[root_j] = root_i
+
+def get_groups(root_list):
+    groups = {}
+
+    for i in range(len(root_list)):
+        i_root = find(i, root_list)
+        if i_root in groups.keys():
+            groups[i_root].append(i)
         else:
-            incidence[p[0]] = {p[1]}
-
-        if p[1] in incidence.keys():
-            incidence[p[1]].add(p[0])
-        else:
-            incidence[p[1]] = {p[0]}
-
-    return incidence
-
-def build_groups(incidence, s_len):
-    visited = set()
-    groups = []
-
-    for idx in range(s_len):
-        if idx not in visited:
-            current_group = {idx}
-            queue = [idx]
-            visited.add(idx)
-
-            while len(queue) > 0:
-                edges = incidence.get(queue.pop(0), [])
-
-                for e in edges:
-                    if e not in visited:
-                        current_group.add(e)
-                        visited.add(e)
-                        queue.append(e)
-                    else:
-                        continue
-
-            current_group = list(current_group)
-            current_group.sort()
-
-            groups.append(current_group)
+            groups[i_root] = [i]
 
     return groups
-
-
-            
-
-
-        
